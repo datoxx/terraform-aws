246053dc6ab7b872fc41dc61190bfe7180adeea8
datoxx <davit.ardzenadze.05@gmail.com>
2023-07-29 01:31:42 +0400

    add, run docker on ec2

--- script.sh
+++ script.sh
@@ -4,6 +4,3 @@ sudo systemctl start docker
 sudo usermod -aG docker ec2-user
 docker run -p 8080:80 nginx
 
-# sudo yum update -y && sudo yum install -y docker
-# sudo service docker start
-# sudo docker run -p 8080:80 nginx
