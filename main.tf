672723e6d192a9058b85bd4267b2893da846ef38
datoxx <davit.ardzenadze.05@gmail.com>
2023-07-28 15:18:08 +0400

    add security group

--- main.tf
+++ main.tf
@@ -8,6 +8,9 @@ variable vpc_cidr_block {}
 variable subnet_1_cidr_block {}
 variable avail_zone {}
 variable env_prefix {}
+variable my_ip {}
+
+
 
 
 # start, add custom vpc and subnet inside it 
@@ -60,6 +63,38 @@ resource "aws_route_table" "myapp-route-table" {
 resource "aws_route_table_association" "a-rtb-subnet" {
   subnet_id      = aws_subnet.myapp-subnet-1.id
   route_table_id = aws_route_table.myapp-route-table.id
+}
+
+# create security group
+resource "aws_security_group" "myapp-sg" {
+  name   = "myapp-sg"
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.my_ip]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-sg"
+  }
 }
 
 
